cdcd5e42036468db4517571a23d9f58aea15af63
Daniel Orner <daniel.orner@flipp.com>
2015-08-25 13:50:23 -0400

    Add preprocess option

--- activerecord-import.gemspec
+++ activerecord-import.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = "activerecord-import"
-  s.version = "0.2.13.wishabi"
+  s.version = "0.2.14.flipp"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Zach Dennis"]
--- lib/activerecord-import/import.rb
+++ lib/activerecord-import/import.rb
@@ -254,6 +254,9 @@ class ActiveRecord::Base
         instance = new do |model|
           hsh.each_pair{ |k,v| model.send("#{k}=", v) }
         end
+        if options[:preprocess]
+          options[:preprocess].call(instance)
+        end
         if not instance.valid?
           array_of_attributes[ i ] = nil
           failed_instances << instance
